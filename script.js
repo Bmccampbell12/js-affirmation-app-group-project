bc6e2d1a2913614ca7b680af07b4b3714da4c013
Bruce McCampbell <mccampbellbruce1@gmail.com>
2024-09-06 14:19:26 -0500

    js-affirmation-app-group-project

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ onReady();
 function onReady() {
   console.log('Javascript is working!');
 }
+
+let tableBody = document.getElementById("table") ;
+let emojiDiv = document.getElementById("emojiDiv")
+function makeFire(){
+  emojiDiv.innerHTML += "<span onClick= deleteFire()>🔥</span>"
+}
+
+function makeIce(){
+  emojiDiv.innerHTML += "<span onClick= deleteIce()>❄️</span>"
+}
+
+function deleteAffirmation(event){
+  let parentRow = event.target.parentElement
+  parentRow.remove()
+}
+
+// delete function should be used along with 
+
+function submitText(){
+  let deleteButton = `<td onClick="deleteAffirmation(event)">❌</td>`
+  let formAffirmation = document.getElementById("affirmation").value  
+  let formAuthor = document.getElementById("Author").value   
+  tableBody.innerHTML +=   `<tr><td>${formAffirmation}</td><td>${formAuthor}</td>${deleteButton}</tr>`
+  event.preventDefault();
+} 
